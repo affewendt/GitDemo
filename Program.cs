8a6b53f7daea9d5c924ecf69dbd6b95a6d8c962a
Alfred Wendt <affewendt@gmail.com>
2026-09-11 09:40:49 +0200

    Added a new method

--- Program.cs
+++ Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main()
     {
         Console.WriteLine("Hello Git!");
+        Console.WriteLine(DoSomething("Alfred"));
+    }
+
+
+    static string DoSomething(string name)
+    {
+        return $"Hello {name}";
     }
 }
